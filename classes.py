b958cf35844c1f3731b7b295e105a78fa8ffd060
Pantelis <vafeidis@campus.tu-berlin.de>
2019-10-11 15:55:32 -0700

    Amacrine cell and time-specific output

--- classes.py
+++ classes.py
@@ -7,13 +7,14 @@ from scipy import signal
 image = (200, 200)  # in um
 pixel = 5           # in um
 temporal_res = .1  # in msec
+t_time = 100        # in sec
 
 
 # Generic mother class for anything that is common across the building blocks 
 # (elements) of the circuits
 
 class Element:
-    
+  
     # Every element contains the name of its inputs and the corresponding weights
     def __init__(self, inputs, weights):
         # Inputs: array of input elements to this element
@@ -24,10 +25,12 @@ class Element:
         # Am I allowed to pass an array of objects? I want to compute their
         # output and used it is the output method
         self.inputs = inputs
+        
+        # preallocate matrix of activity for this cell
+        self.output = np.full((np.ceil(t_time/temporal_res*1000),),np.nan)
         # Can be used to see if the cell has computed its output, to avoid
         # uneccesary computations. Can also be replaced by a time marker if the
         # network cannot be computed all at once. We do not focus on simulation for now
-        self.output = np.nan
         
     
 class BipolarCell(Element):
@@ -71,17 +74,51 @@ class BipolarCell(Element):
         self.activation = attributes["activation"]
         self.threshold = attributes["threshold"]
     
-    def output(self,image,t):
+    def output(self,t):
         # Since there is no recurrent connectivity involving bipolar cells,
         # we can compute all the output and then sample it from the other cells
+        # Caveat: Figure 1 of Gollisch2010 involves recurrent connections
+        # between bipolar and amacrine cells, but this can be taken into account
+        # later if needed
         if not np.isnan(self.output):
             pass
         else:
-            temp = signal.fftconvolve(image,self.spatiotemporal,axes=2)
+            # assuming that inputs is always a cell array, the first cell should
+            # contain the image
+            temp = signal.fftconvolve(self.inputs[0],self.spatiotemporal,axes=2)
             self.output = activation(temp,self.activation,self.threshold)
         
         return self.output[t]
+    
+    
+class AmacrineCell(Element):
+    
+    def __init__(self, inputs, weights, attributes):
+        # Attributes: contains a list of attributes needed to define the cell. Can contain:
+        #       temporal: contains temporal receptive field or function name to produce it
+        #       duration: "time constant" of the temporal receptive field
+        #       activation: the nonlinear activation function of the output
+        #       threshold: threshold for the activation function
+        #   Can also contain other parameters required to define the receptive
+        #   field, look at respective function for what their name should be
         
+        super().__init__(inputs, weights)
+        
+        self.activation = attributes["activation"]
+        self.threshold = attributes["threshold"]
+        
+    def output(self,t):
+        # Amacrine cells receive recurrent connections
+
+        # assuming that inputs is a cell array of input objects
+        if not np.isnan(self.output[t]):
+            pass
+        else:
+            values = self.inputs.output[t]  # I do not think this is going to work, see how to modify
+            temp = np.dot(values,self.w)
+            self.output[t] = activation(temp,self.activation,self.threshold)
+        
+        return self.output[t]
 
 
 def Spatial(attributes):
@@ -152,4 +189,3 @@ def relu(h,threshold,gain = 1):
 def sigmoid(h,threshold,k=.5,b=5,s=1):
     # could define k, b and s separately for each cell
     return 1/(1+k*np.exp(-b*(h-threshold)))
-    
