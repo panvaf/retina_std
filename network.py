91819ab4edb303e9f3381abeceefb2bf95ad5f89
Pantelis <vafeidis@campus.tu-berlin.de>
2019-10-24 17:58:27 -0700

    Network initialization

--- network.py
+++ network.py
@@ -18,9 +18,14 @@ def network_init(structs,size):
     global image_size, pixel
     img_size = [round(image_size[0]/pixel),round(image_size[1]/pixel)]
     neurons = {}
+    celltypes = ['BipolarCell','AmacrineCell','GanglionCell','Delay','PresynapticSilencer']
     
     for key, value in size.items():
         
+        for temp in celltypes:
+                    if temp in key:
+                        celltype = temp
+                        
         neuron_list = [[None]*value]*value
         x = np.linspace(1, img_size[0]-1, value)
         y = np.linspace(1, img_size[1]-1, value)
@@ -33,6 +38,8 @@ def network_init(structs,size):
                 
                 # counter used to delete connections if at the edge of the image
                 counter = 0
+                weights = struct["weigths"]
+                attributes = struct["attributes"]
                 # Build connections of this neuron
                 center = np.asarray([XX[i,j],YY[i,j]])
                 inputs = []
@@ -40,32 +47,38 @@ def network_init(structs,size):
                 # Parse through different cell types to find appropriate cells,
                 # using the size of their grid
                 
-                # need to create a different procedure for bipolar cells, which
-                # have only the image as an input
-                for input_type in struct["inputs"]:
+                if celltype == 'BipolarCell':
+                    inputs = struct['inputs']
+                else:
                     
-                    connectivity = struct["connectivity"][input_type]
-                    temp = center*size[input_type]/value; corr_center = temp.astype(int)
+                    for input_type in struct["inputs"]:
+                        
+                        connectivity = struct["connectivity"][input_type]
+                        temp = center*size[input_type]/value; corr_center = temp.astype(int)
+                        
+                        for coords in connectivity:
+                            
+                            input_coords = corr_center + coords
+                            
+                            if all(0 < input_coords < size[input_type]):
+                                
+                                inputs.append(neurons[input_type][input_coords[0]][input_coords[1]])
+                                
+                            else:
+                                # If coordinates point out of grid:
+                                # delete corresponding weight etc to not mess up everything
+                                # also delete corresponding coeffs, duration and temporal arguments
+                                weights = np.delete(weights,counter)
+                                del attributes["temporal"][counter]
+                                del attributes["duration"][counter]
+                                del attributes["coeffs"][counter]
+                                # arrays now are one element shorter
+                                counter = counter - 1
+                                
+                        counter = counter + 1
+                        
+                neuron_list[i][j] = celltype(inputs,weights,center,attributes)
                     
-                    for coords in connectivity:
-                        
-                        input_coords = corr_center + coords
-                        
-                        if all(0 < input_coords < size[input_type]):
-                            
-                            inputs.append(neurons[input_type][input_coords[0]][input_coords[1]])
-                            
-                        else:
-                            
-                            # delete corresponding weight etc to not mess up everything
-                            
-                
-                
-                # If a certain neuron not found, abort the corresponding weight
-                
-                
-        
-        
         neurons[key] = neuron_list
     
     return neurons
